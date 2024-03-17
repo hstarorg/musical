ad09100445c4b3fcf647456a5d2d30c14aff1881
hstarorg <hm910705@163.com>
2024-03-17 17:11:50 +0800

    feat: init project with react-native cli new version

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,7 @@
 module.exports = {
-  semi: true,
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
-  printWidth: 120,
 };
--- app/navigators/index.ts
+++
@@ -1,2 +0,0 @@
-export { AppNavigator, AppNavigationProps } from './AppNavigator';
-export * from './navigationUtilities';
